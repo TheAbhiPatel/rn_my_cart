92dde303b5afb23e6957d3e528a4fb732069421a
TheAbhiPatel <theabhipate.co@gmail.com>
2023-08-25 15:58:13 +0530

    Add : icons and export from index file

---
+++ src/icons/index.ts
@@ -0,0 +1,4 @@
+export const UserIcon = require('./user.png');
+export const CartIcon = require('./cart.png');
+export const HomeIcon = require('./home.png');
+export const CategoryIcon = require('./categories.png');
