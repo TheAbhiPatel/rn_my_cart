14c6a2f505bc764755771c57e1efa4d93048df0c
TheAbhiPatel <theabhipate.co@gmail.com>
2023-08-25 14:27:07 +0530

    Add : provider in app and use theme and make a screen and test theme

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {ThemeProvider} from '@shopify/restyle';
+import theme from './src/theme/light';
+import Box from './src/theme/Box';
+import Text from './src/theme/Text';
+import Home from './src/screens/home/Home';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <Box
+        flex={1}
+        bg="$Background"
+        justifyContent="center"
+        alignItems="center">
+        <Text color="$primary">App</Text>
+        <Home />
+      </Box>
+    </ThemeProvider>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
